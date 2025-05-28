a9126a299eaa3351728925e6e6eab70627eef332
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-05-27 22:26:14 -0400

    Start setting up obstacle tool

---
+++ raccoon-tools-editor-lite/src/app/components/obstacle/obstacle-card/obstacle-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obstacle-card',
+  imports: [],
+  templateUrl: './obstacle-card.component.html',
+  styleUrl: './obstacle-card.component.scss'
+})
+export class ObstacleCardComponent {
+
+}
---
+++ raccoon-tools-editor-lite/src/app/components/obstacle/obstacle-list/obstacle-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obstacle-list',
+  imports: [],
+  templateUrl: './obstacle-list.component.html',
+  styleUrl: './obstacle-list.component.scss'
+})
+export class ObstacleListComponent {
+
+}
