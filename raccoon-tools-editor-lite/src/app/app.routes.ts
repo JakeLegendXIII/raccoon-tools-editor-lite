4c80f6b4fd0904f5338992ca623f6b6738a4c6f4
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-05-31 19:28:58 -0400

    Simplify viewer to just be visualizer instead

--- raccoon-tools-editor-lite/src/app/app.routes.ts
+++ raccoon-tools-editor-lite/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LevelViewerComponent } from './components/level-viewer/level-viewer.component';
+// import { LevelViewerComponent } from './components/level-viewer/level-viewer.component';
 import { PlayerListComponent } from './components/player/player-list/player-list.component';
 import { EnemyListComponent } from './components/enemy/enemy-list/enemy-list.component';
 import { ObstacleListComponent } from './components/obstacle/obstacle-list/obstacle-list.component';
@@ -7,8 +7,8 @@ import { ExportComponent } from './components/export/export.component';
 import { VisualizerComponent } from './components/visualizer/visualizer.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/level-viewer', pathMatch: 'full' },
-  { path: 'level-viewer', component: LevelViewerComponent },
+  { path: '', redirectTo: '/visualizer', pathMatch: 'full' },
+  // { path: 'level-viewer', component: LevelViewerComponent },
   { path: 'player-list', component: PlayerListComponent },
   { path: 'enemy-list', component: EnemyListComponent },
   { path: 'obstacle-list', component: ObstacleListComponent },
