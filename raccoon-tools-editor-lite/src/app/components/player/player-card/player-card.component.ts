d961e4cadf1c12c102c7aa0b1f9991d2869270c2
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-01 13:48:16 -0400

    Update player-card to have dropdown enabled on editing for player type

--- raccoon-tools-editor-lite/src/app/components/player/player-card/player-card.component.ts
+++ raccoon-tools-editor-lite/src/app/components/player/player-card/player-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 import { Store } from '@ngrx/store';
 import { BasePlayerType, PlayerData } from '../../../models/level.model';
 import { updatePlayer, deletePlayer } from '../../../store/level.actions';
@@ -19,63 +20,74 @@ import { updatePlayer, deletePlayer } from '../../../store/level.actions';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSelectModule
   ],
   standalone: true,
   templateUrl: './player-card.component.html',
   styleUrl: './player-card.component.scss'
 })
-export class PlayerCardComponent {
+export class PlayerCardComponent implements OnInit {
   @Input() player!: PlayerData;
   
   private store = inject(Store);
   isEditing = false;
   editablePlayer!: PlayerData;
 
-  BasePlayerType = BasePlayerType;
+  // Cache the player type options to avoid recalculation
+  playerTypeOptions: Array<{ value: number; name: string }> = [];
 
   ngOnInit(): void {
     this.resetEditablePlayer();
+    this.initializePlayerTypeOptions();
   }
 
-  toggleEdit(): void {
-    if (this.isEditing) {
-      this.cancelEdit();
-    } else {
-      this.startEdit();
-    }
-  }
-
-  startEdit(): void {
-    this.isEditing = true;
-    this.editablePlayer = { ...this.player };
-    // Copy the StartPosition object separately to avoid reference issues
-    this.editablePlayer.StartPosition = { ...this.player.StartPosition };
-  }
-
-  saveChanges(): void {
-    this.store.dispatch(updatePlayer({ player: this.editablePlayer }));
-    this.isEditing = false;
-  }
-  cancelEdit(): void {
-    this.isEditing = false;
-    this.resetEditablePlayer();
-  }
-
-  deletePlayer(): void {
-    if (confirm(`Are you sure you want to delete Player ${this.player.ID}?`)) {
-      this.store.dispatch(deletePlayer({ playerId: this.player.ID }));
-    }
+  private initializePlayerTypeOptions(): void {
+    this.playerTypeOptions = Object.entries(BasePlayerType)
+      .filter(([key, value]) => typeof value === 'number')
+      .map(([key, value]) => ({
+        value: value as number,
+        name: key
+      }));
   }
 
   getPlayerTypeName(playerType: number): string {
     return BasePlayerType[playerType] || 'Unknown';
   }
 
-  private resetEditablePlayer(): void {
-    this.editablePlayer = { ...this.player };
-    if (this.player?.StartPosition) {
-      this.editablePlayer.StartPosition = { ...this.player.StartPosition };
+  getPlayerTypeOptions(): Array<{ value: number; name: string }> {
+    return this.playerTypeOptions;
+  }
+
+  toggleEdit(): void {
+    this.isEditing = !this.isEditing;
+    if (this.isEditing) {
+      this.resetEditablePlayer();
+    }
+  }
+
+  resetEditablePlayer(): void {
+    if (this.player) {
+      this.editablePlayer = {
+        ...this.player,
+        StartPosition: { ...this.player.StartPosition }
+      };
+    }
+  }
+
+  saveChanges(): void {
+    this.store.dispatch(updatePlayer({ player: this.editablePlayer }));
+    this.isEditing = false;
+  }
+
+  cancelEdit(): void {
+    this.isEditing = false;
+    this.resetEditablePlayer();
+  }
+
+  deletePlayer(): void {
+    if (confirm('Are you sure you want to delete this player?')) {
+      this.store.dispatch(deletePlayer({ playerId: this.player.ID }));
     }
   }
 }
