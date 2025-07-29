808180cdd590b7e187e32aa48fe1844f08513709
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-07-29 13:22:49 -0400

    Add conversion unit tests and cleanup app tests

--- raccoon-tools-editor-lite/src/app/components/conversion/conversion.component.spec.ts
+++ raccoon-tools-editor-lite/src/app/components/conversion/conversion.component.spec.ts
@@ -20,4 +20,23 @@ describe('ConversionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should convert ounces to milliliters correctly', () => {
+    component.beforeOunces = 1;
+    expect(component.beforeML).toBeCloseTo(29.5735, 4);
+    
+    component.afterOunces = 2;
+    expect(component.afterML).toBeCloseTo(59.147, 4);
+
+    expect(component.differenceML).toBeCloseTo(29.5735, 4);
+  });
+
+  it('should handle early base case correctly', () => {
+    component.beforeOunces = 95.23
+    component.afterOunces = 96.47;
+
+    expect(component.beforeML).toBeCloseTo(2816.28, 1);
+    expect(component.afterML).toBeCloseTo(2852.96, 1);
+    expect(component.differenceML).toBeCloseTo(36.67, 1);
+  });
 });
