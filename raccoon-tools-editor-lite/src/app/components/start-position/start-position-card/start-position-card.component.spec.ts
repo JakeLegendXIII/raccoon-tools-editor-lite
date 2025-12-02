b3224d50248a923d57949f3e0e101d1ccd4be211
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-12-01 21:22:15 -0500

    Update tests

--- raccoon-tools-editor-lite/src/app/components/start-position/start-position-card/start-position-card.component.spec.ts
+++ raccoon-tools-editor-lite/src/app/components/start-position/start-position-card/start-position-card.component.spec.ts
@@ -1,19 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { StartPositionCardComponent } from './start-position-card.component';
 
 describe('StartPositionCardComponent', () => {
   let component: StartPositionCardComponent;
   let fixture: ComponentFixture<StartPositionCardComponent>;
+  let store: MockStore;
+
+  const initialState = {
+    level: {
+      currentLevel: {
+        ID: 0,
+        GridWidth: 8,
+        GridHeight: 8,
+        CellSize: 64,
+        LevelType: 0,
+        LevelDescription: '',
+        NumberOfTurns: 0,
+        WinPosition: { X: 0, Y: 0 },
+        StartPositionsList: [],
+        Players: [],
+        Enemies: [],
+        Obstacles: []
+      }
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StartPositionCardComponent]
+      imports: [StartPositionCardComponent],
+      providers: [provideMockStore({ initialState })]
     })
     .compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(StartPositionCardComponent);
     component = fixture.componentInstance;
+    
+    // Set required inputs
+    component.startPosition = { X: 0, Y: 0 };
+    component.index = 0;
+    
     fixture.detectChanges();
   });
 
--- raccoon-tools-editor-lite/src/app/components/start-position/start-position-list/start-position-list.component.spec.ts
+++ raccoon-tools-editor-lite/src/app/components/start-position/start-position-list/start-position-list.component.spec.ts
@@ -1,17 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { StartPositionListComponent } from './start-position-list.component';
 
 describe('StartPositionListComponent', () => {
   let component: StartPositionListComponent;
   let fixture: ComponentFixture<StartPositionListComponent>;
+  let store: MockStore;
+
+  const initialState = {
+    level: {
+      currentLevel: {
+        ID: 0,
+        GridWidth: 8,
+        GridHeight: 8,
+        CellSize: 64,
+        LevelType: 0,
+        LevelDescription: '',
+        NumberOfTurns: 0,
+        WinPosition: { X: 0, Y: 0 },
+        StartPositionsList: [],
+        Players: [],
+        Enemies: [],
+        Obstacles: []
+      }
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StartPositionListComponent]
+      imports: [StartPositionListComponent],
+      providers: [provideMockStore({ initialState })]
     })
     .compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(StartPositionListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
