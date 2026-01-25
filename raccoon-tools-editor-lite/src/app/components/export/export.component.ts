55e9bd5eca625d9d0dadf5216ac76ab77987d86f
JakeLegendXIII <jakelegendxiii@gmail.com>
2026-01-24 23:47:26 -0500

    Update Export to allow for Items export

--- raccoon-tools-editor-lite/src/app/components/export/export.component.ts
+++ raccoon-tools-editor-lite/src/app/components/export/export.component.ts
@@ -3,13 +3,18 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 import { Level } from '../../models/level.model';
+import { Item } from '../../models/item.model';
 import { selectCurrentLevel } from '../../store/level.selectors';
+import { selectItems } from '../../store/items.selectors';
+
+export type ExportType = 'level' | 'items';
 
 @Component({
   selector: 'app-export',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MatButtonToggleModule],
   templateUrl: './export.component.html',
   styleUrls: ['./export.component.scss']
 })
@@ -17,10 +22,18 @@ export class ExportComponent {
   private store = inject(Store);
 
   currentLevel$: Observable<Level | null>;
+  items$: Observable<Item[]>;
   fileName: string = 'level';
+  exportType: ExportType = 'level';
 
   constructor() {
     this.currentLevel$ = this.store.select(selectCurrentLevel);
+    this.items$ = this.store.select(selectItems);
+  }
+
+  onExportTypeChange(): void {
+    // Update default filename based on export type
+    this.fileName = this.exportType === 'level' ? 'level' : 'items';
   }
 
   exportLevel() {
@@ -31,7 +44,15 @@ export class ExportComponent {
     }).unsubscribe();
   }
 
-  private downloadJSON(data: Level, filename: string) {
+  exportItems() {
+    this.items$.subscribe(items => {
+      if (items && items.length > 0) {
+        this.downloadJSON(items, this.fileName);
+      }
+    }).unsubscribe();
+  }
+
+  private downloadJSON(data: Level | Item[], filename: string) {
     const jsonStr = JSON.stringify(data, null, 2);
     const blob = new Blob([jsonStr], { type: 'application/json' });
     const url = window.URL.createObjectURL(blob);
